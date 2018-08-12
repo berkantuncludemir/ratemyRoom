443190ff291e5a99b799b27289b9b55c892fffc1
Behran Kankul <behrank@gmail.com>
2018-08-12 23:48:10 +0300

    navbar added

--- src/Header.js
+++ src/Header.js
@@ -1,24 +1,51 @@
 import React, { Component } from 'react';
-import {Navbar,Nav,NavItem,MenuItem,NavDropdown} from 'react-bootstrap'
 import './navbar.css';
 
 class Header extends React.Component {
     render() {
         return (
-            //fixed-top navbar-expand-lg navbar-light
-            <Navbar fixedTop>
-                <Navbar.Brand>
-                    <a href="#home">|RooM|</a>
-                </Navbar.Brand>
-                <Navbar.Collapse>
-                    <Nav>
-                        <Nav.NavItem eventKey={1} href="#">
-                            Home
-                        </Nav.NavItem>
-                    </Nav>
-                </Navbar.Collapse>
-            </Navbar>
-        )
+            <nav class="navbar fixed-top navbar-expand-lg navbar-light">
+                <a class="navbar-brand" href="#">|RooM|</a>
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent"
+                    aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item active">
+                            <a class="nav-link" href="#">Home
+                            <span class="sr-only">(current)</span>
+                            </a>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true"
+                                aria-expanded="false">
+                                Categories
+                            </a>
+                            <div class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                <a class="dropdown-item" href="#">Living Room</a>
+                                <a class="dropdown-item" href="#">Bedroom</a>
+                                <a class="dropdown-item" href="#">Kitchen</a>
+                                <a class="dropdown-item" href="#">Dining Room</a>
+                                <a class="dropdown-item" href="#">Bathroom</a>
+                                <a class="dropdown-item" href="#">Kids Room</a>
+                                <div class="dropdown-divider"></div>
+                                <a class="dropdown-item" href="#">Show All</a>
+                            </div>
+                        </li>
+                        <li class="nav-item dropdown">
+                            <form class="form-inline my-2 my-lg-0">
+                                <button class="btn btn-danger navbar-btn">Upload Photos</button>
+                            </form>
+                        </li>
+                    </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="search" placeholder="profiles, rooms, items..." aria-label="Search" />
+                        <button class="btn btn-outline-success my-2 my-sm-0" type="submit">Search</button>
+                     </form>
+                </div>
+            </nav>
+                );
     }
 }
 
--- src/index.js
+++ src/index.js
@@ -5,6 +5,6 @@ import App from './App';
 import Header from './Header'
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<Header />,document.getElementById('nav'));
+ReactDOM.render(<Header />,document.getElementById('navbar'));
 //ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
